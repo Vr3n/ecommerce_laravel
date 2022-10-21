eff2405c4982908fb8bd38c5019b5f3c50fb280d
Vr3n <vickyspatel@gmail.com>
2022-10-21 23:38:27 +0530

    User Profile and Addresses Table Created.
    Fix #4

---
+++ database/migrations/2022_10_21_170057_create_user_addresses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('building');
+            $table->string('street')->nullable();
+            $table->string('landmark')->nullable();
+            $table->string('city');
+            $table->string('district')->nullable();
+            $table->string('state');
+            $table->string('pin_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_addresses');
+    }
+};
