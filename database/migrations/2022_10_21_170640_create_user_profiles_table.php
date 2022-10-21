08f667b8a9419473c12f02444e676c846a4301cb
Vr3n <vickyspatel@gmail.com>
2022-10-22 00:17:39 +0530

    Default Profile Picture to user profile added.
    Fix: #7

--- database/migrations/2022_10_21_170640_create_user_profiles_table.php
+++ database/migrations/2022_10_21_170640_create_user_profiles_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('user_profiles', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('profile_picture')->nullable();
+            $table->string('profile_picture')->default('default_profile_picture.png');
             $table->foreignId('user_addresses_id')->nullable()->constrained()->onDelete('cascade');
             $table->timestamps();
         });
